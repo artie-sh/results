e11eda3f22d7e9e6a2a87c1f063df355a5414768
artie-sh <artie@blackswan-technologies.com>
2020-06-04 03:14:44 +0200

    display class show hide escape done

--- app/display.py
+++ app/display.py
@@ -6,8 +6,7 @@ from app.read_data import DataReader
 class Display():
     main_bg = 'grey'
     right_clicks = 0
-    cells = {}
-    # cells = []
+    cells = []
     dr = None
     master = None
 
@@ -26,26 +25,33 @@ class Display():
         frame.pack(expand=True)
 
         for i in range(self.height): #Rows
+            row = []
             for j in range(self.width): #Columns
                 text = self.results_data[i][j]
                 fg = 'black' if i == 0 else self.main_bg
                 b = Label(frame, text=text, fg=fg, font=('Arial', 32, 'bold'), relief=RIDGE, width=10, bg=self.main_bg)
                 b.grid(row=i, column=j)
-                self.cells[(i,j)] = b
+                row.append(b)
+            self.cells.append(row)
 
         self.master.bind('<Right>', lambda event: self.show_next_record(event))
+        self.master.bind('<Left>', lambda event: self.hide_last_record(event))
+        self.master.bind('<Escape>', lambda event: self.close(event))
 
     def show_next_record(self, event):
-        if self.right_clicks < self.height - 1:
-            for j in range(self.width):
-                self.cells[(self.height - self.right_clicks - 1, j)].config(fg='black')
-            self.right_clicks += 1
+        for cell in self.cells[-self.right_clicks-1]:
+            cell.config(fg='black')
+        self.right_clicks += 1
 
-    def hide_previous_record(self, event):
-        if self.right_clicks < self.height - 1:
-            for j in range(self.width):
-                self.cells[(self.height - self.right_clicks - 1, j)].config(fg='black')
-            self.right_clicks += 1
+    def hide_last_record(self, event):
+        if not self.right_clicks == 0:
+            for cell in self.cells[-self.right_clicks]:
+                cell.config(fg=self.main_bg)
+            self.right_clicks -= 1
+
+    def close(self, event):
+        self.master.withdraw()  # if you want to bring it back
+        sys.exit()
 
 
 Display()
